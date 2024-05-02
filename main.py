c9ce37a6d8db81a131d2ade74139699afdfa142d
hardwaremc <167475971+hardwaremc@users.noreply.github.com>
2024-05-02 23:22:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+def rock_paper_scissors():
+  user = input("Escolha pedra, papel ou tesoura: ")
+  computer = random.choice(['pedra', 'papel', 'tesoura'])
+  print(f"Computer escolheu {computer}")
+
+  if user == computer:
+    return "Empate!"
+
+  elif (user == "pedra" and computer == "tesoura" or (user == "papel" and computer == "pedra")) or (user == "tesoura" and computer == "papel"):
+    return "Você ganhou!"
+  else:
+    return "Você perdeu!"
+
+print(rock_paper_scissors())
+
+
